02ddefe63df55fc664cd9d8b464695c36fcbebae
Cristian <cmartinez@clicktotravel.es>
2022-07-01 16:32:31 +0200

    Command update: extract constants

--- app/Console/Commands/SuspiciousDetectorCommand.php
+++ app/Console/Commands/SuspiciousDetectorCommand.php
@@ -8,14 +8,16 @@ use Throwable;
 
 class SuspiciousDetectorCommand extends Command
 {
+    private const NAME_FILE_ARGUMENT_NAME = 'nameFile';
+    private const RESULT_TABLE_HEADERS = ['Client', 'Month', 'Suspicious', "Median"];
 
-    protected $signature = 'suspicious:detector {nameFile}';
-    protected $description = 'Command description';
+    protected $signature = 'suspicious:detector {'. self::NAME_FILE_ARGUMENT_NAME .'}';
+    protected $description = 'Detect suspicious readings and print a table with the results.';
 
 
     public function handle(): void
     {
-        $nameFile = $this->argument('nameFile');
+        $nameFile = $this->argument(self::NAME_FILE_ARGUMENT_NAME);
 
         $this->info('File name input: ' . $nameFile);
 
@@ -30,11 +32,11 @@ class SuspiciousDetectorCommand extends Command
 
     }
 
-    private function printResultsTable(array $results): void
+    private function printResultsTable(array $rows): void
     {
         $this->table(
-            ['Client', 'Month', 'Suspicious', "Median"],
-            $results
+            self::RESULT_TABLE_HEADERS,
+            $rows
         );
     }
 }
